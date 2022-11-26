f62b05227697b4216b02f0e46a3648d5453fefc2
Olga Parich <test1-ole4ka@yandex.ru>
2022-11-26 11:44:56 +0300

    Add README.md with a description of the tests and tests.py with unit-tests for the class BooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # qa_python
+Проверки для конструктора:
+    Проверка что books_rating - словарь. `test_books_rating_is_dictionary`
+    Проверка что favorites - список. `test_favorites_is_list`
+
+Проверки методов:
+    Проверка что дефолтный рейтинг у только что добавленой книги =1. `test_add_new_book_default_rating`
+    Проверка что рейтинг от 1 до 10 устанавливается. `test_set_book_rating_set_rating8_true`
+    Проверка что рейтинг больше 10 не устанавливается. `test_set_book_rating_rating11_false`
+    Проверка получения рейтинга по имени книги. `test_get_book_rating_added_book_true`
+    Проверка получения книг с определенным рейтингом рейтингом. `test_get_books_whith_specific_rating_raiting8_true`
+    Проверка получения книг с определенным рейтингом из пустого списка. `test_get_books_whith_specific_rating_list_empty`
+    Проверка получения списка из трех книг. `test_get_books_rating_three_books_true`
+    Проверка добавления в Избранное книги которая есть в общем списке. `test_add_book_in_favorites_book_in_rating_true`
+    Проверка добавления в избранное книги которой нет в общем списке. `test_add_book_in_favorites_book_not_from_rating_false`
+    Проверка добавления в Избранное книги которая уже есть в списке Избранного. `test_add_book_in_favorites_book_already_in_favorites_false`
+    Проверка удаления книги из Избранного. `test_delete_book_from_favorites_book_in_favirites_true`
+    Проверка удаления из Избранного книги которой нет в Избранном. `test_delete_book_from_favorites_book_not_from_favorites_false`
+    Проверка получения Избранного. `test_get_list_of_favorites_books_two_books_true`
--- tests.py
+++ tests.py
@@ -1,13 +1,8 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
     # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -17,8 +12,113 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    #Проверяем что books_rating - словарь
+    def test_books_rating_is_dictionary(self):
+        collector = BooksCollector()
+        assert collector.books_rating == {}
+
+    #Проверяем что favorites - список
+    def test_favorites_is_list(self):
+        collector = BooksCollector()
+        assert collector.favorites == []
+
+    #Проверяем что при добавлении новой книги её дефолтный рейтинг =1
+    def test_add_new_book_default_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Зов кукушки')
+        assert collector.get_books_rating()['Зов кукушки'] == 1
+
+    #Проверяем установку корректного рейтинга
+    def test_set_book_rating_set_rating8_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и Мир')
+        collector.set_book_rating('Война и Мир', 8)
+        assert collector.get_books_rating()['Война и Мир'] == 8
+
+    #Проверяем установки не корректного рейтинга книге
+    def test_set_book_rating_rating11_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и Мир')
+        collector.set_book_rating('Война и Мир', 11)
+        assert collector.get_books_rating()['Война и Мир'] == 1
+
+    #Проверяем получение рейтинга по имени книги
+    def test_get_book_rating_added_book_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Повелитель мух')
+        collector.set_book_rating('Повелитель мух', 5)
+        assert collector.get_book_rating('Повелитель мух') == 5
+
+    # Проверяем получение книг с рейтингом 8
+    def test_get_books_whith_specific_rating_raiting8_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Хоббит')
+        collector.set_book_rating('Хоббит', 7)
+        collector.add_new_book('Властелин колец')
+        collector.set_book_rating('Властелин колец', 8)
+        books_with_specific_rating = collector.get_books_with_specific_rating(8)
+        assert len(books_with_specific_rating) == 1
+
+    # Проверяем получение книг из пустого списка
+    def test_get_books_whith_specific_rating_list_empty(self):
+        collector = BooksCollector()
+        books_with_specific_rating = collector.get_books_with_specific_rating(8)
+        assert books_with_specific_rating == []
+
+    # Проверяем получение списка из трех книг
+    def test_get_books_rating_three_books_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Игра престолов')
+        collector.add_new_book('Пир стервятников')
+        collector.add_new_book('Битва королей')
+        assert len(collector.get_books_rating()) == 3
+
+    #Проверяем добавление книги которая есть в общем списке в избранное
+    def test_add_book_in_favorites_book_in_rating_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    #Проверяем добавление в избранное книги которой нет в общем списке
+    def test_add_book_in_favorites_book_not_from_rating_false(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Властелин колец')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    #Проверяем добавление книги которая уже есть в списке избранного
+    def test_add_book_in_favorites_book_already_in_favorites_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Сияние')
+        collector.add_book_in_favorites('Сияние')
+        collector.add_book_in_favorites('Сияние')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    #Проверяем удаление книги из избранного
+    def test_delete_book_from_favorites_book_in_favirites_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Миссия шута')
+        collector.add_new_book('Судьба шута')
+        collector.add_book_in_favorites('Миссия шута')
+        collector.add_book_in_favorites('Судьба шута')
+        collector.delete_book_from_favorites('Судьба шута')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    #Проверяем удаление книги которой нет в избранном
+    def test_delete_book_from_favorites_book_not_from_favorites_false(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.add_book_in_favorites('Оно')
+        collector.delete_book_from_favorites('Кэри')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    #Проверяем получение Избранного
+    def test_get_list_of_favorites_books_two_books_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.add_book_in_favorites('Оно')
+        collector.add_new_book('Сияние')
+        collector.add_book_in_favorites('Сияние')
+        assert len(collector.get_list_of_favorites_books()) == 2
